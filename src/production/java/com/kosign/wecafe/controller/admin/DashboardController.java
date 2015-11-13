991ead037395f49b425c0b8c518779bc0db0a17a
darapenhchet <darapenhchet@gmail.com>
2015-11-13 16:01:21 +0700

    DASHBOARD

--- src/production/java/com/kosign/wecafe/controller/admin/DashboardController.java
+++ src/production/java/com/kosign/wecafe/controller/admin/DashboardController.java
@@ -6,10 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.kosign.wecafe.services.OrderProductService;
+import com.kosign.wecafe.services.SellProductsService;
 import com.kosign.wecafe.services.SellService;
 import com.kosign.wecafe.services.UserService;
-import com.kosign.wecafe.services.SellProductsService;
 
 @Controller
 public class DashboardController {
@@ -18,17 +17,14 @@ public class DashboardController {
 	UserService userService;
 	
 	@Autowired
-	SellService sellService;
-	
-	@Autowired
-	OrderProductService orderProductService;
-	
-	@Autowired
 	SellProductsService sellProductService;
+	
+	@Autowired
+	SellService sellerService;
 	
 	@RequestMapping(value={"/admin/dashboard","/admin/","/admin/home"})
 	public String dashboard(Map<String, Object> model){
-		model.put("TOTAL_SALES", userService.getAllUsers().size());
+		model.put("TOTAL_SALES", sellerService.getSellAllList().size());
 		model.put("NEW_ORDERS", sellProductService.getOrdered().size());
 		model.put("TOTAL_USERS", userService.getAllUsers().size());
 		return "admin/dashboard";
