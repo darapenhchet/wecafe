765a2c4e2bc55502cfd0daad23ebdd2778803bf1
Channbora <Channbora@channbora>
2015-11-13 14:17:55 +0700

    order size in dashboard

--- src/production/java/com/kosign/wecafe/controller/admin/DashboardController.java
+++ src/production/java/com/kosign/wecafe/controller/admin/DashboardController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.kosign.wecafe.services.OrderProductService;
 import com.kosign.wecafe.services.SellService;
 import com.kosign.wecafe.services.UserService;
+import com.kosign.wecafe.services.SellProductsService;
 
 @Controller
 public class DashboardController {
@@ -22,10 +23,13 @@ public class DashboardController {
 	@Autowired
 	OrderProductService orderProductService;
 	
+	@Autowired
+	SellProductsService sellProductService;
+	
 	@RequestMapping(value={"/admin/dashboard","/admin/","/admin/home"})
 	public String dashboard(Map<String, Object> model){
 		model.put("TOTAL_SALES", userService.getAllUsers().size());
-		model.put("NEW_ORDERS", 100);
+		model.put("NEW_ORDERS", sellProductService.getOrdered().size());
 		model.put("TOTAL_USERS", userService.getAllUsers().size());
 		return "admin/dashboard";
 	}
--- webapp/WEB-INF/jsp/views/admin/importupdate.jsp
+++ webapp/WEB-INF/jsp/views/admin/importupdate.jsp
@@ -530,7 +530,7 @@
     				    type: 'POST', 
     				    dataType: 'JSON', 
     				    beforeSend: function(xhr) {
-    	                    xhr.setRequestHeader("Accept", "application/json");
+    	                    xhr.setRequestHeader("Accept", "application/json1 ");
     	                    xhr.setRequestHeader("Content-Type", "application/json");
     	                },
     				    success: function(data) { 
