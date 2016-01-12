be4ce58328d947a5f42c825eb4668b7f9721124c
Channbora <Channbora@channbora>
2016-01-12 08:08:40 +0700

    importproductcontroller

---
+++ src/production/java/com/kosign/wecafe/controller/admin/importProductController.java
@@ -0,0 +1,124 @@
+package com.kosign.wecafe.controller.admin;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.kosign.wecafe.entities.ImportDetail;
+import com.kosign.wecafe.entities.ImportProduct;
+import com.kosign.wecafe.entities.Pagination;
+import com.kosign.wecafe.entities.Product;
+import com.kosign.wecafe.entities.ProductFilter;
+import com.kosign.wecafe.entities.Supplier;
+import com.kosign.wecafe.forms.ImportForm;
+import com.kosign.wecafe.services.ImportService;
+import com.kosign.wecafe.services.ProductService;
+import com.kosign.wecafe.services.SupplierService;
+
+@Controller
+public class importProductController {
+	
+	@Inject ImportService importService;
+	
+	@Inject ProductService productService;
+	
+	@Inject SupplierService supplierService;
+	
+	@RequestMapping(value="/admin/importlist", method=RequestMethod.GET)
+	public String listAllImpProduct(Map<String, Object>model){
+		//model.put("importproducts",importService.listAllImportProduct());
+		return "admin/importlist";
+	}
+	
+	@RequestMapping(value="/admin/getimportlist", method=RequestMethod.GET) 
+	public ResponseEntity<Map<String, Object>> getAllProducts(ProductFilter filter, Pagination pagination,
+			@RequestParam(value="start_date") String strStartDate, @RequestParam(value="end_date") String strEndDate) throws ParseException{
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		Date startDate = simpleDateFormat.parse(strStartDate);
+		Date endDate = simpleDateFormat.parse(strEndDate);
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("imports", importService.listAllImportProduct(pagination, startDate,endDate));
+		map.put("total_amount", importService.getTotalAmount(startDate, endDate));
+		pagination.setTotalCount(importService.count());
+		pagination.setTotalPages(pagination.totalPages());
+		map.put("pagination",pagination);
+		return new ResponseEntity<Map<String, Object>>(map, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value="/admin/searchproduct", method=RequestMethod.POST)
+	public @ResponseBody List<Product> searchProductName(){
+		return importService.listAllProduct();
+	}
+	
+	@RequestMapping(value="/admin/searchsupplier", method=RequestMethod.POST)
+	public @ResponseBody List<Supplier> searchSupplierName(){
+		return importService.listAllSupplier();
+	}
+	
+	@RequestMapping(value="/admin/importadd", method=RequestMethod.GET)
+	public String impProductAdd(){
+		return "admin/importadd";
+	}
+	
+	@RequestMapping(value="/admin/addImport", method=RequestMethod.POST, consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody Boolean saveImpProduct(@RequestBody List<ImportForm> importForm)	{
+		ImportDetail importDetails = new ImportDetail();
+		System.out.println(importForm.size());
+		try {			
+			return importService.saveImportPro(importForm);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	@RequestMapping(value="/admin/importupdate/{id}", method=RequestMethod.POST, consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody Boolean importupdate(@RequestBody List<ImportForm> importform, @PathVariable("id") Long impId){
+		
+		System.out.println("simpId +" + impId);
+		 try {
+			return importService.updateImportPro(importform, impId);
+			//return importService.deleteImportPro(importform, impId);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}  
+		return false;
+	}
+	
+	@RequestMapping(value="admin/getimportdetail/{id}", method=RequestMethod.POST)
+		public @ResponseBody List<Map> importdetail(@PathVariable("id") Long impid, Map<String, Object>model){
+			   
+		return importService.listAllImportDetail(impid);
+			
+		}
+	 
+	@RequestMapping(value="/admin/viewById/{id}", method=RequestMethod.GET)
+	public String viewById(@PathVariable("id") Long impid, Map<String, Object>model){
+		 
+		model.put("importpro", importService.listAllImportDetail(impid));
+		model.put("IMPORT_ID", impid);
+		  
+		 
+		return "admin/importupdate";
+	}
+	
+
+}
