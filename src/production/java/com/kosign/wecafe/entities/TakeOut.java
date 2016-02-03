dd35b08638eb8414fc92ae13f28ef15c66fc0977
Channbora <Channbora@channbora>
2016-02-03 09:21:30 +0700

    take out stock

--- src/production/java/com/kosign/wecafe/entities/TakeOut.java
+++ src/production/java/com/kosign/wecafe/entities/TakeOut.java
@@ -1,16 +1,17 @@
-/*package com.kosign.wecafe.entities;
+package com.kosign.wecafe.entities;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
  
 @Entity
-@Table(name="takeout")
+@Table(name="takeout_stock")
 public class TakeOut {
 	
-	@Column(name="t_id")
-	private long tId;
+	@Column(name="pro_id")
+	private long proId;
 	
+	@Column(name="qty")
+	private long qty;
 //	private 
 }
-*/
